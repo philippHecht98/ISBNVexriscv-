66c626deeb54cfd1e34857885f917ef07d6898c3
= <=>
2022-02-16 14:23:11 +0100

    edited vexriscv with isbn config

--- src/main/scala/vexriscv/ExtendedISBN.scala
+++ src/main/scala/vexriscv/ExtendedISBN.scala
@@ -11,75 +11,48 @@ import spinal.core._
 object ExtendedISBN extends App{
   def config = VexRiscvConfig(
     plugins = List(
-      new IBusCachedPlugin(
-        prediction = DYNAMIC,
-        config = InstructionCacheConfig(
-          cacheSize = 4096,
-          bytePerLine =32,
-          wayCount = 1,
-          addressWidth = 32,
-          cpuDataWidth = 32,
-          memDataWidth = 32,
-          catchIllegalAccess = true,
-          catchAccessFault = true,
-          asyncTagMemory = false,
-          twoCycleRam = true,
-          twoCycleCache = true
+        new IBusSimplePlugin(
+          resetVector = 0x80000000l,
+          cmdForkOnSecondStage = false,
+          cmdForkPersistence = false,
+          prediction = NONE,
+          catchAccessFault = false,
+          compressedGen = false
         ),
-        memoryTranslatorPortConfig = MmuPortConfig(
-          portTlbSize = 4
-        )
-      ),
-      new DBusCachedPlugin(
-        config = new DataCacheConfig(
-          cacheSize         = 4096,
-          bytePerLine       = 32,
-          wayCount          = 1,
-          addressWidth      = 32,
-          cpuDataWidth      = 32,
-          memDataWidth      = 32,
-          catchAccessError  = true,
-          catchIllegal      = true,
-          catchUnaligned    = true
+        new DBusSimplePlugin(
+          catchAddressMisaligned = false,
+          catchAccessFault = false
         ),
-        memoryTranslatorPortConfig = MmuPortConfig(
-          portTlbSize = 6
-        )
-      ),
-      new MmuPlugin(
-        virtualRange = _(31 downto 28) === 0xC,
-        ioRange      = _(31 downto 28) === 0xF
-      ),
-      new DecoderSimplePlugin(
-        catchIllegalInstruction = true
-      ),
-      new RegFilePlugin(
-        regFileReadyKind = plugin.SYNC,
-        zeroBoot = false
-      ),
-      new IntAluPlugin,
-      new SrcPlugin(
-        separatedAddSub = false,
-        executeInsertion = true
-      ),
-      new FullBarrelShifterPlugin,
-      new HazardSimplePlugin(
-        bypassExecute           = true,
-        bypassMemory            = true,
-        bypassWriteBack         = true,
-        bypassWriteBackBuffer   = true,
-        pessimisticUseSrc       = false,
-        pessimisticWriteRegFile = false,
-        pessimisticAddressMatch = false
-      ),
-      new MulPlugin,
-      new CsrPlugin(CsrPluginConfig.small(0x80000020l)),
-      new BranchPlugin(
-        earlyBranch = false,
-        catchAddressMisaligned = true
-      ),
-      new YamlPlugin("cpu0.yaml"),
-      new ISBNPlugin
+        new CsrPlugin(CsrPluginConfig.smallest),
+        new DecoderSimplePlugin(
+          catchIllegalInstruction = false
+        ),
+        new RegFilePlugin(
+          regFileReadyKind = plugin.SYNC,
+          zeroBoot = false
+        ),
+        new IntAluPlugin,
+        new MulPlugin,
+        new SrcPlugin(
+          separatedAddSub = false,
+          executeInsertion = false
+        ),
+        new LightShifterPlugin,
+        new HazardSimplePlugin(
+          bypassExecute           = false,
+          bypassMemory            = false,
+          bypassWriteBack         = false,
+          bypassWriteBackBuffer   = false,
+          pessimisticUseSrc       = false,
+          pessimisticWriteRegFile = false,
+          pessimisticAddressMatch = false
+        ),
+        new BranchPlugin(
+          earlyBranch = false,
+          catchAddressMisaligned = false
+        ),
+        new YamlPlugin("cpu0.yaml"),
+        new ISBNPlugin()
     )
   )
 
